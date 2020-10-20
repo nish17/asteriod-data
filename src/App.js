cce006889b062ea8fc35e022e69995d4b9b78f57
Nimesh Solanki <nimesh3536@gmail.com>
2020-10-20 23:59:30 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
+import Dashboard from './Components/Dashboard';
 
-import { getTodaysData } from './api';
-import AstroCard from './Components/AstroCard';
+import Header from './Components/Header';
+
 function App() {
-  const [APIData, setAPIData] = useState({});
-
-  useEffect(() => {
-    async function getData() {
-      const response = await getTodaysData();
-      const dateKey = Object.keys(response.near_earth_objects)[0];
-      setAPIData(response.near_earth_objects[dateKey]);
-    }
-    getData();
-  }, []);
-
   return (
     <div className='App'>
-      Asteriod Data
-      {APIData.length > 0 &&
-        APIData.map((data, i) => 
-        <AstroCard data={data} />
-        // <div key={i}>{data['name']}</div>
-        )}
+      <Header />
+      <Dashboard />
     </div>
   );
 }
