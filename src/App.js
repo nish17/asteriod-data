a464daa6dccdee1d05d8ffd2aae2957a804bccb3
Nimesh Solanki <nimesh3536@gmail.com>
2020-10-20 13:22:13 +0530

    make use of astrocard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import { getTodaysData } from './api';
-
+import AstroCard from './Components/AstroCard';
 function App() {
   const [APIData, setAPIData] = useState({});
 
@@ -13,12 +13,16 @@ function App() {
       setAPIData(response.near_earth_objects[dateKey]);
     }
     getData();
-  },[]);
+  }, []);
 
   return (
     <div className='App'>
-        Asteriod Data
-      {APIData.length > 0 && APIData.map((data,i) => <div key={i}>{data['name']}</div>)}
+      Asteriod Data
+      {APIData.length > 0 &&
+        APIData.map((data, i) => 
+        <AstroCard data={data} />
+        // <div key={i}>{data['name']}</div>
+        )}
     </div>
   );
 }
