278c16a7de3bf54bd0d1c90099e7c7a05662b423
Nimesh Solanki <nimesh3536@gmail.com>
2020-10-21 00:37:32 +0530

    add grids

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+
 import './App.css';
 import Dashboard from './Components/Dashboard';
-
 import Header from './Components/Header';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <Dashboard />
+      <Grid container direction='column'>
+        <Grid item>
+          <Header />
+        </Grid>
+        <Grid item container>
+          <Grid item xs={0} sm={2} />
+          <Grid item xs={12} sm={8}>
+            <Dashboard />
+          </Grid>
+          <Grid item xs={0} sm={2} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
