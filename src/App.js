dd6c8efd4b2f5b6768c8898387fea10f93905e8a
Nimesh Solanki <nimesh3536@gmail.com>
2020-10-22 00:52:31 +0530

    change xs value

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
           <Header />
         </Grid>
         <Grid item container>
-          <Grid item xs={0} sm={2} />
+          <Grid item xs={1} sm={2} />
           <Grid item xs={12} sm={8}>
             <Dashboard />
           </Grid>
-          <Grid item xs={0} sm={2} />
+          <Grid item xs={1} sm={2} />
         </Grid>
       </Grid>
     </div>
